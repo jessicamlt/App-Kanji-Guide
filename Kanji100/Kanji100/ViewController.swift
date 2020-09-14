6707608de27eadd8ecc4c46259ffebfcc943f926
Jéssica Trindade <jessica.mlt@hotmail.com>
2020-09-14 14:18:38 -0300

    Criação do Projeto

---
+++ Kanji100/Kanji100/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kanji100
+//
+//  Created by Jéssica Trindade on 14/09/20.
+//  Copyright © 2020 Jéssica Trindade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
