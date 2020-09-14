cd80f632f049772c38e6bfe1822cc3ef52a9da20
Jéssica Trindade <jessica.mlt@hotmail.com>
2020-09-14 15:24:19 -0300

    Criação das Views

---
+++ Kanji100/Kanji100/MainScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainScreenViewController.swift
+//  Kanji100
+//
+//  Created by Jéssica Trindade on 14/09/20.
+//  Copyright © 2020 Jéssica Trindade. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
