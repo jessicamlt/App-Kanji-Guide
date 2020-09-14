12cc99cd7ffedbcdefb96a0b051972efa124b80d
Jéssica Trindade <jessica.mlt@hotmail.com>
2020-09-14 14:38:29 -0300

    Criação da estrutura de Kanjis

---
+++ Kanji100/Kanji100/ Kanji.swift
@@ -0,0 +1,23 @@
+//
+//   Kanji.swift
+//  Kanji100
+//
+//  Created by Jéssica Trindade on 14/09/20.
+//  Copyright © 2020 Jéssica Trindade. All rights reserved.
+//
+
+import Foundation
+
+struct Kanji: Decodable {
+    var kanji: String?
+    var englishMeaning: String?
+    var onyomy: String?
+    var kunyomi: String?
+    var category: String?
+    var exemples: [Word]?
+}
+
+struct Word: Decodable {
+    var word: String?
+    var englishMeaning: [String]?
+}
