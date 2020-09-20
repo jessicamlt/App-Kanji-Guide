69dfd57d24e8909ba2f23a4b6924eefd6125ee5d
Jéssica Trindade <jessica.mlt@hotmail.com>
2020-09-20 11:33:52 -0300

    Criação da tela de favoritos

---
+++ Kanji100/Kanji100/FavoriteScreen/FavoriteScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteScreenViewController.swift
+//  Kanji100
+//
+//  Created by Jéssica Trindade on 20/09/20.
+//  Copyright © 2020 Jéssica Trindade. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
