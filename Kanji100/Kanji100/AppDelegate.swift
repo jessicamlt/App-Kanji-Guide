757273ed22f67502ebf37fde1b663c4df327a864
Jessica <jessica.mlt@hotmail.com>
2020-10-06 10:24:02 -0300

    Update AppDelegate.swift

--- Kanji100/Kanji100/AppDelegate.swift
+++ Kanji100/Kanji100/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func createScreen() -> UINavigationController {
+    private func createScreen() -> UINavigationController {
         let favoriteManager = FavoriteManager()
         let mainScreen = MainScreenViewController.create(favoriteManager: favoriteManager)
         let mainScreenNavigationController = UINavigationController(rootViewController: mainScreen)
@@ -28,24 +28,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         mainScreenNavigationController.navigationBar.barTintColor = UIColor(named: "second")
         return mainScreenNavigationController
     }
-    
-//    func createMainScreen(favoriteManager: FavoriteManager) -> UINavigationController {
-//        let model = MainScreenModel(favoriteManager: favoriteManager)
-//        let mainScreenViewController = MainScreenViewController(favoriteManager: favoriteManager, model: model)
-//        let mainScreenNavigationController = UINavigationController(rootViewController: mainScreenViewController)
-//        mainScreenNavigationController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-//        mainScreenNavigationController.navigationBar.barTintColor = UIColor(named: "second")
-//        return mainScreenNavigationController
-//    }
-    
-//    func createFavoriteScreen(favoriteManager: FavoriteManager) -> UINavigationController {
-//        let model = FavoriteScreenModel(favoriteManager: favoriteManager)
-//        let favoriteScreenViewController = FavoriteScreenViewController(favoriteManager: favoriteManager, model: model)
-//        let favoriteScreenNavigationController = UINavigationController(rootViewController: favoriteScreenViewController)
-//        favoriteScreenNavigationController.title = "Favorites"
-//        favoriteScreenNavigationController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-//        favoriteScreenNavigationController.navigationBar.barTintColor = UIColor(named: "second")
-//        
-//        return favoriteScreenNavigationController
-//    }
 }
